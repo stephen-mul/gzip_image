2a5ccae85f8d7316c8234224eaffc66044ae0201
Stephen Mulligan <smulligan@pp.studio>
2023-07-24 14:29:51 +0200

    Typo in calling np.zeros

--- experiment.py
+++ experiment.py
@@ -11,7 +11,8 @@ if __name__ == '__main__':
     args = parser.parse_args()
     if config.experiment_name=='TRAIN_SIZE':
         train_size_list = config.train_size_list
-        result_array = np.zeros(len(train_size_list), 3)
+        headers = ['Correct', 'Incorrect', 'Training Set Size']
+        result_array = np.zeros((len(train_size_list), len(headers)))
         row_count = 0
         for train_size in train_size_list:
             print(f'Testing with training set of size {train_size} and testing set of size' 
@@ -20,7 +21,6 @@ if __name__ == '__main__':
             result_array[row_count, 0] = hit
             result_array[row_count, 1] = miss
             result_array[row_count, 2] = train_size
-        headers = ['Correct', 'Incorrect', 'Training Set Size']
         out_name = args.name
         if not os.path.exists('./results'):
             os.makedirs('./results')
